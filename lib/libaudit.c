660ee15a20cf0b1b6dc4f9b5f6a3e90905684269
sgrubb <sgrubb@03a675c2-f56d-4096-908f-63dba836b7e4>
2011-10-27 13:16:54 +0000

    Some code cleanups
    git-svn-id: http://svn.fedorahosted.org/svn/audit/branches/1.8@598 03a675c2-f56d-4096-908f-63dba836b7e4

--- lib/libaudit.c
+++ lib/libaudit.c
@@ -1120,9 +1120,6 @@ int audit_rule_fieldpair_data(struct audit_rule_data **rulep, const char *pair,
 				|| flags == AUDIT_FILTER_ENTRY))
 				return -17;
 			
-			if (flags == AUDIT_FILTER_EXCLUDE)
-				return -18;
-
 			if (!isdigit((char)*(v)))
 				return -21;
 
